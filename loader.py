7e36cd5b1f0a5009be7ad43d34a4a88a7e5afd08
jonnyjackson <jonny@jonnyjxn.co>
2020-10-17 02:28:41 +0100

    Added threadsafe version

--- loader.py
+++ loader.py
@@ -25,15 +25,17 @@ class MeshDataset(_MonaiDataset):
 	Each mesh is wrapped as a PyRender mesh object.
 	"""
 
-	def __init__(self, mesh_paths, image_transform=[]):
+	def __init__(self, mesh_paths, image_transform=[], use_threadsafe=False):
 		"""
 		Args:
 			mesh_paths (dict): A dictionary where the key is an index and the value is a string containing a path to a .stl file
 		"""        
 		self.meshes = {}
 
+		_RenderImage = ThreadsafeRenderImage if use_threadsafe else RenderImage
+
 		self.transform = Compose([
-			RenderImage(),
+			_RenderImage(),
 			*image_transform
 		])
 
@@ -138,6 +140,83 @@ class RenderImage(object):
 		return image.astype(np.uint8)  
 
 
+class ThreadsafeRenderImage(object):
+	"""
+	The ThreadsafeRenderImage transform renders an image or multiple images of the passed mesh using the PyRender library.
+	This thread-safe version does not persist the pyrender.Scene, pyrender.OffscreenRenderer, etc. objects but instead
+	creates local versions on each __call__
+	"""     
+
+	def __init__(self, image_size=[250,250], pixel_size=[.25,.25], focal_length=[4100,4100], sequence_type="list",
+				ppa_list=[0,20], psa_list=[0,0], ppa_range=[-20,20], psa_range=[0,0], seed=0, n_images=2):
+		"""
+		Args:
+			mesh_paths (dict): A dictionary where the key is an index and the value is a string containing a path to a .stl file
+		"""
+		self.image_size = image_size
+		self.pixel_size = pixel_size
+		self.focal_length = focal_length
+
+		self.sequence_type = sequence_type # either "list" or "random"
+		self.ppa_list = ppa_list
+		self.psa_list = psa_list
+		self.ppa_range = ppa_range
+		self.psa_range = psa_range
+		self.seed = seed
+		self.n_images = n_images
+
+		if sequence_type == "list":
+			assert len(ppa_list) == len(psa_list), f"'ppa_list' dimension must match 'psa_list': {len(ppa_list)} vs {len(psa_list)}"
+			self.n_images = len(ppa_list)
+		elif sequence_type == "random":
+			#TODO: implement random generation using seed
+			pass
+
+	def _instantiate_pyrender_scene(self):
+		scene = pyrender.Scene()
+		renderer = pyrender.OffscreenRenderer(self.image_size[0],self.image_size[1])
+
+		light = pyrender.DirectionalLight(color=np.ones(3), intensity=1.0)
+		cam = pyrender.IntrinsicsCamera(
+				fx=self.focal_length[0],
+				fy=self.focal_length[0],
+				cx=self.image_size[0]/2,
+				cy=self.image_size[1]/2,
+				zfar=100000000000000 # `Infinite` clipping
+			)
+
+		light_obj = scene.add(light)
+		cam_obj = scene.add(cam)
+
+		return scene, renderer, light_obj, cam_obj
+
+	def __call__(self, mesh):
+		scene, renderer, light_obj, cam_obj = self._instantiate_pyrender_scene()
+
+		mesh_obj = scene.add(mesh)
+
+		imgs = []
+		for i in range(self.n_images):
+			pose = get_matrix(self.ppa_list[i], self.psa_list[i], self.pixel_size[0]*self.focal_length[0])
+			imgs.append(
+				self._get_one_image(pose, scene, renderer, light_obj, cam_obj)
+			)
+
+		scene.remove_node(mesh_obj)
+
+		return np.dstack(imgs)
+
+	def _get_one_image(self, pose, scene, renderer, light_obj, cam_obj):
+		scene.set_pose(light_obj, pose)
+		scene.set_pose(cam_obj, pose)
+
+		image_rgb, depth = renderer.render(scene)
+
+		# Convert to single channel (greyscale)
+		image = np.array(image_rgb[:, :, 0])*.299 + np.array(image_rgb[:, :, 1])*.587 + np.array(image_rgb[:, :, 2])*.114
+		return image.astype(np.uint8)  
+
+
 root_path = Path("../CoronaryVesselGeneration/AngioGenAppNew/output/testing/")
 
 keys = range(100)
@@ -146,7 +225,7 @@ values = [str(root_path / f"{(i+1):04}" / "mesh.stl") for i in keys]
 meshes = dict(zip(keys, values))
 dataset = MeshDataset(meshes)
 
-dataloader = DataLoader(dataset, batch_size=100, num_workers=0)
+dataloader = DataLoader(dataset, batch_size=100, num_workers=2)
 
 import time
 from PIL import Image
